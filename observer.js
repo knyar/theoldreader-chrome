eabd19632156f71427c8b3098f6d8adb8ce56a41
Anton Tolchanov <me@knyar.net>
2013-06-08 22:48:12 +0100

    Update changelog, adjust indenting style

--- observer.js
+++ observer.js
@@ -13,18 +13,17 @@ observer.observe(target, { subtree: true, characterData: true, childList: true }
 function notify(mutation) {
   var title = mutation.target.textContent;
   var count;
-  
+
   var match = /^\((\d+)\)/.exec(title);
   var match_zero = /^The Old Reader$/.exec(title); // Does not fire on navigation
-  
+
   if (match && match[1]) {
     count = match[1];
-  }
-  else if (match_zero) {
+  } else if (match_zero) {
     count = 0;
   }
-  
-  if(typeof count !== 'undefined'){
+
+  if (typeof count !== 'undefined') {
     console.log("Observer reported "+count+" to extension");
     chrome.extension.sendMessage({'count' : count});
   }
--- options.js
+++ options.js
@@ -1,27 +1,27 @@
 function save_options() {
-	localStorage['click_page'] = $('#click_page').val();
-	localStorage['show_notifications'] = $('#show_notifications').prop('checked') ? 'yes' : 'no';
+  localStorage['click_page'] = $('#click_page').val();
+  localStorage['show_notifications'] = $('#show_notifications').prop('checked') ? 'yes' : 'no';
   localStorage['prefer_https'] = $('#prefer_https').prop('checked') ? 'yes' : 'no';
-	$('#save_notification').fadeIn('fast').delay(2000).fadeOut('fast');
+  $('#save_notification').fadeIn('fast').delay(2000).fadeOut('fast');
 }
 
 function load_options() {
-	if (localStorage['click_page']) {
-		$('#click_page').val(localStorage['click_page']);
-	}
-	if (localStorage['show_notifications'] == 'yes') {
-		$('#show_notifications').prop('checked', true);
-	}
-	if (localStorage['prefer_https'] == 'yes') {
-		$('#prefer_https').prop('checked', true);
-	}
+  if (localStorage['click_page']) {
+    $('#click_page').val(localStorage['click_page']);
+  }
+  if (localStorage['show_notifications'] == 'yes') {
+    $('#show_notifications').prop('checked', true);
+  }
+  if (localStorage['prefer_https'] == 'yes') {
+    $('#prefer_https').prop('checked', true);
+  }
 }
 
 $(document).ready(function() {
-	$('#save_button').click(function() {
-		save_options();
-		return false;
-	});
+  $('#save_button').click(function() {
+    save_options();
+    return false;
+  });
 });
 
 document.addEventListener('DOMContentLoaded', load_options);
