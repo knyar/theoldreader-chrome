a57a0fc2bb91d42308eef9e2f72f543141a2acd4
Anton Tolchanov <me@knyar.net>
2013-06-09 00:48:03 +0100

    Remove intents

--- intent-subscribe.js
+++
@@ -1,6 +0,0 @@
-// vim: set ts=2 sw=2 et
-window.addEventListener("load", function() {
-  if (window.webkitIntent) {
-    window.location = 'http://theoldreader.com/feeds/subscribe?url=' + window.webkitIntent.data
-  }
-}, false);
