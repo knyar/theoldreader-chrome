863da7f50d926d765342fb2b0f16287ddaec19fa
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-07-12 21:16:29 +0300

    Get rid from *://*/ permission

--- menu.js
+++ menu.js
@@ -1,4 +1,26 @@
 var parent = chrome.contextMenus.create({"title": "The Old Reader"});
+var bookmark = function bookmark(base_url, url, selection) {
+  var f = document.createElement('form');
+  f.setAttribute('method','post');
+  f.setAttribute('action',base_url);
+
+  var f1 = document.createElement('input');
+  f1.setAttribute('type','hidden');
+  f1.setAttribute('name','saved_post[url]');
+  f1.setAttribute('value',url);
+  f.appendChild(f1);
+
+  if (selection) {
+    var f2 = document.createElement('input');
+    f2.setAttribute('type','hidden');
+    f2.setAttribute('name','saved_post[content]');
+    f2.setAttribute('value',selection);
+    f.appendChild(f2);
+  }
+
+  document.getElementsByTagName('body')[0].appendChild(f);
+  f.submit();
+}.toString().replace(/(\n|\t)/gm,'');
 
 chrome.contextMenus.create({
   "title": "Subscribe to page",
@@ -16,7 +38,10 @@ chrome.contextMenus.create({
   "parentId": parent,
   contexts: ["page"],
   "onclick": function(info, tab) {
-    chrome.tabs.executeScript(tab.id, { file: "bookmarklet.js" });
+    var args = "\""+baseUrl()+"bookmarks/bookmark\",\""+info.pageUrl+"\"";
+    chrome.tabs.create({
+      url: "javascript:"+bookmark+";bookmark("+args+")"
+    })
   }
 });
 
@@ -24,6 +49,11 @@ chrome.contextMenus.create({
   "title": "Bookmark selection",
   contexts: ["selection"],
   "onclick": function(info, tab) {
-    chrome.tabs.executeScript(tab.id, { file: "bookmarklet.js" });
+    var selection = info.selectionText.replace(/\"/gm, '\\"');
+    var args = "\""+baseUrl()+"bookmarks/bookmark\",\""+info.pageUrl+"\", \""+selection+"\"";
+    console.log(args);
+    chrome.tabs.create({
+      url: "javascript:"+bookmark+";bookmark("+args+")"
+    })
   }
 });
